aa44d3344388123f0438a4a3b0a21df127a68885
jardel sewo <hcjardes@gmail.com>
2023-12-04 16:40:16 -0300

    envio de imagens cartoon minha

--- src/portfolio.js
+++ src/portfolio.js
@@ -447,7 +447,7 @@ const publications = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "jardelc.png",
+    profile_image_path: "jardel_cartoon.png",
     description:
       "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
   },
